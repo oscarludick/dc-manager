207e1dfcedab8d6327596bc19d6940719d27c64b
Oscar Ivan Ludick Guadarrama <oscarludick@gmail.com>
2021-09-23 12:22:11 -0500

    fix(dc-manager-app): fix missing files

---
+++ dc-manager-app/src/app/app.reducer.ts
@@ -0,0 +1,17 @@
+import { SeriesModel } from './core/series/series.model';
+
+export interface AppState {
+  series: {
+    current: SeriesModel;
+    list: ReadonlyArray<SeriesModel[]>;
+  };
+  comics: {
+    current: any;
+    list: ReadonlyArray<any[]>;
+  };
+}
+
+export const AppReducers = {
+  series: null,
+  comics: null,
+};
